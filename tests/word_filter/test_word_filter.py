eccdbedb4b418240ed5639db433513402b381f72
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-03 09:53:51 +0900

    [refactor] word filter

--- tests/word_filter/test_word_filter.py
+++ tests/word_filter/test_word_filter.py
@@ -2,5 +2,5 @@ from word_filter.word_filter import WordFilter
 
 
 def test_word_filter():
-    filter = WordFilter("target_value")
+    filter = WordFilter("ng_word")
     assert filter
--- word_filter/word_filter.py
+++ word_filter/word_filter.py
@@ -1,3 +1,16 @@
+"""ワードフィルタ"""
+
+
 class WordFilter:
+    """ワードフィルタ"""
+
     def __init__(self, ng_word: str):
+        """
+        初期化
+
+        Parameters
+        ----------
+        ng_word : str
+            フィルタする単語
+        """
         pass
