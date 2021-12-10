a7f7fdb7254b997a826b843bd31f95544d9d93e4
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-10 09:23:39 +0900

    [add] word filter class (#1)

---
+++ tests/word_filter/test_word_filter.py
@@ -0,0 +1,6 @@
+from word_filter.word_filter import WordFilter
+
+
+def test_word_filter():
+    filter = WordFilter("ng_word")
+    assert filter
---
+++ word_filter/word_filter.py
@@ -0,0 +1,16 @@
+"""ワードフィルタ"""
+
+
+class WordFilter:
+    """ワードフィルタ"""
+
+    def __init__(self, ng_word: str):
+        """
+        初期化
+
+        Parameters
+        ----------
+        ng_word : str
+            フィルタする文字列
+        """
+        pass
