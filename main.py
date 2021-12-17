02c6f75ff80ada63f80d85295ed5e07ecb77ea0c
keitakawakami <81949636+keitakawakami@users.noreply.github.com>
2021-12-17 09:49:34 +0900

    不要なファイル削除 (#2)
    * 不要なファイル削除
    
    * main.py削除

--- main.py
+++
@@ -1,9 +0,0 @@
-"""main"""
-from logging import config
-
-from word_filter.huga import Huga
-
-config.fileConfig("logging.conf", disable_existing_loggers=False)
-
-if __name__ == "__main__":
-    Huga().piyo()
