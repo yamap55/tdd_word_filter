31b0ca9a4a89030b9d81007147996c8d2d31b740
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-24 09:08:51 +0900

    [add] test

--- tests/word_filter/test_word_filter.py
+++ tests/word_filter/test_word_filter.py
@@ -4,3 +4,11 @@ from word_filter.word_filter import WordFilter
 def test_word_filter():
     filter = WordFilter("ng_word")
     assert filter
+
+
+def test_detect():
+    filter = WordFilter("ng_word")
+    actual = filter.detect("hoge ng_word huga.")
+
+    expected = True
+    assert actual == expected
